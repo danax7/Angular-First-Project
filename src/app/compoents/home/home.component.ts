5e9b0dd875ad8ba95b31d42912cd6d6ff439023d
Daniil Khakhulin <dan.praeco@gmail.com>
2024-01-03 14:40:54 +0700

    added local db server and updated services for async calls

--- src/app/compoents/home/home.component.ts
+++ src/app/compoents/home/home.component.ts
@@ -18,9 +18,27 @@ import { HousingService } from '../../services/housing/housing.service';
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
